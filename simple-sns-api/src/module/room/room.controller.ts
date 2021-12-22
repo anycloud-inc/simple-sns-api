993b0c5f95c3e61c37dc967cbac22953bf634137
KazutakaShimizu <s.kazutaka55555@gmail.com>
2021-12-22 16:37:27 +0900

    ルームの一覧取得API

--- simple-sns-api/src/module/room/room.controller.ts
+++ simple-sns-api/src/module/room/room.controller.ts
@@ -1,11 +1,24 @@
 import { Request, Response, NextFunction } from 'express'
 import { Auth } from 'src/lib/auth'
-import { Controller, Post } from 'src/lib/controller'
+import { Controller, Get, Post } from 'src/lib/controller'
 import { roomSerializer } from './room.serializer'
 import { roomService } from './room.service'
 
 @Controller('/rooms')
 export class RoomController {
+  @Get()
+  @Auth
+  async index(req: Request, res: Response, next: NextFunction): Promise<void> {
+    try {
+      const rooms = await roomService.findRooms(req.currentUser.id!)
+      res.json({
+        rooms: rooms.map(x => roomSerializer.build(x)),
+      })
+    } catch (e) {
+      next(e)
+    }
+  }
+
   @Post()
   @Auth
   async create(req: Request, res: Response, next: NextFunction): Promise<void> {
