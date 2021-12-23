c08e573ba2e6a2f2b104d5530e231b50f8fbb0dc
KazutakaShimizu <s.kazutaka55555@gmail.com>
2021-12-23 10:55:48 +0900

    メッセージ取得API

--- simple-sns-api/src/module/message/message.controller.ts
+++ simple-sns-api/src/module/message/message.controller.ts
@@ -1,15 +1,37 @@
 import { Request, Response, NextFunction } from 'express'
 import { Auth } from 'src/lib/auth'
-import { Controller, Post } from 'src/lib/controller'
+import { Controller, Get, Post } from 'src/lib/controller'
+import { getPaginationParams } from 'src/lib/request-utils'
+import { roomPolicy } from '../room/room.policy'
 import { messageSerializer } from './message.serializer'
 import { messageService } from './message.service'
 
 @Controller('/messages')
 export class MessageController {
+  @Get()
+  @Auth
+  async index(req: Request, res: Response, next: NextFunction): Promise<void> {
+    try {
+      const pagination = getPaginationParams(req.query)
+      const roomId = req.query.roomId as string
+
+      await roomPolicy.showableOrFail(roomId, req.currentUser.id!)
+
+      const messages = await messageService.findMessages(roomId, pagination)
+      res.json({
+        messages: messages.map(x => messageSerializer.build(x)),
+      })
+    } catch (e) {
+      next(e)
+    }
+  }
+
   @Post()
   @Auth
   async create(req: Request, res: Response, next: NextFunction): Promise<void> {
     try {
+      console.log(req.currentUser.id)
+      console.log(req.body.roomId)
       const message = await messageService.createMessage(req.currentUser.id!, {
         roomId: req.body.roomId,
         content: req.body.content,
